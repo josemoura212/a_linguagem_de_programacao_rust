f5a94288e7e41ba88559bb4cf25b4c7bd1e8081e
José Augusto <josemoura212@gmail.com>
2024-01-09 20:24:20 -0300

    Add area calculation for rectangle

--- src/capitulo5/retangulo.rs
+++ src/capitulo5/retangulo.rs
@@ -1,1 +1,17 @@
-pub fn calcular_area() {}
+pub fn calcular_area() {
+    let rect1 = Retangulo {
+        length: 50,
+        width: 30,
+    };
+
+    println!("A area do retangulo e de {} pixels", area(&rect1));
+}
+
+fn area(retangulo: &Retangulo) -> u32 {
+    retangulo.width * retangulo.length
+}
+
+struct Retangulo {
+    length: u32,
+    width: u32,
+}
